edc9b904ce626d32fa8a514baa10d2707a7770d5
Curtis Ruder HP <curtisjruder@gmail.com>
2022-03-13 15:37:07 -0500

    still messing with the db

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,7 @@
 <?php
     class Database{
-        private $host;
-        private $db_name;
+        private $hostname;
+        private $database;
         private $username;
         private $password;
         private $conn;
@@ -10,15 +10,12 @@
             
             $url = getenv('JAWSDB_URL');
             $dbparts = parse_url($url);
-        
+                       
             $this->hostname = $dbparts['host'];
             $this->username = $dbparts['user'];
             $this->password = $dbparts['pass'];
             $this->database = ltrim($dbparts['path'],'/');
-            //You cannot do the above for your local dev environment, just Heroku
-        
-            // Create your new PDO connection here
-            // This is also from the Heroku docs showing the PDO connection: 
+           
             try {
                 $this->$conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
               // set the PDO error mode to exception
